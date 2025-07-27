7e1e9171efc693d0fb3d58322f561aa564adfc14
FozilAzimov <foziljonweb@gmail.com>
2025-07-27 16:04:06 +0500

    7 kyu added

--- 7kyu.js
+++ 7kyu.js
@@ -89,3 +89,68 @@
 // console.log(multiples(5, 6.0));
 
 // ===========================================================================
+
+// 5.
+// Question:
+// A number m of the form 10x + y is divisible by 7 if and only if x − 2y is divisible by 7.
+// In other words, subtract twice the last digit from the number formed by the remaining digits.
+// Continue to do this until a number known to be divisible by 7 is obtained;
+// you can stop when this number has at most 2 digits because you are supposed to know if a number of at most 2 digits is divisible by 7 or not.
+// The original number is divisible by 7 if and only if the last number obtained using this procedure is divisible by 7.
+// Examples:
+// 1 - m = 371 -> 37 − (2×1) -> 37 − 2 = 35 ; thus, since 35 is divisible by 7, 371 is divisible by 7.
+// The number of steps to get the result is 1.
+// 2 - m = 1603 -> 160 - (2 x 3) -> 154 -> 15 - 8 = 7 and 7 is divisible by 7.
+// 3 - m = 372 -> 37 − (2×2) -> 37 − 4 = 33 ; thus, since 33 is not divisible by 7, 372 is not divisible by 7.
+// 4 - m = 477557101->47755708->4775554->477547->47740->4774->469->28 and 28 is divisible by 7, so is 477557101. The number of steps is 7.
+// Task:
+// Your task is to return to the function seven(m) (m integer >= 0) an array (or a pair, depending on the language) of numbers,
+// the first being the last number m with at most 2 digits obtained by your function (this last m will be divisible or not by 7),
+// the second one being the number of steps to get the result.
+// Forth Note:
+// Return on the stack number-of-steps, last-number-m-with-at-most-2-digits
+// Examples:
+// seven(371) should return [35, 1]
+// seven(1603) should return [7, 2]
+// seven(477557101) should return [28, 7]
+
+// Answare:
+// let seven = (num, count = 0) => {
+//   let newNam = `${num}`;
+//   if (newNam.length > 2) {
+//     let lastVal = +newNam.at(-1);
+//     let currVal = +newNam.slice(0, newNam.length - 1);
+//     let res = currVal - lastVal * 2;
+//     return seven(res, count + 1);
+//   } else {
+//     return [+newNam, count];
+//   }
+// };
+// console.log(seven(1021));
+
+// ===========================================================================
+
+// 6.
+// Question:
+// This is my first Kata in the Ciphers series. This series is meant to test our coding knowledge.
+// Ciphers #1 - The 01 Cipher
+// This cipher doesn't exist, I just created it by myself. It can't actually be used, as there isn't a way to decode it. It's a hash. Multiple sentences may also have the same result.
+// How this cipher works
+// It looks at the letter, and sees it's index in the alphabet, the alphabet being a-z, if you didn't know. If it is odd, it is replaced with 1, if it's even, its replaced with 0. Note that the index should start from 0. Also, if the character isn't a letter, it remains the same.
+// Example
+// encode("Hello World!"); // => "10110 00111!"
+// This is because H's index is 7, which is odd, so it is replaced by 1, and so on.
+// Have fun (en)coding!
+
+// Answare:
+// let encode = (str) => {
+//   let res = "";
+//   for (let val of str.toLowerCase())
+//     val !== val.toUpperCase()
+//       ? (res += (val.charCodeAt() - 97) % 2 ? 1 : 0)
+//       : (res += val);
+//   return res;
+// };
+// console.log(encode("Hello World!"));
+
+// ===========================================================================
