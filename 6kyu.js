1a28e00f8805bc60c6c6ef08e2ceace23a9b687d
FozilAzimov <foziljonweb@gmail.com>
2025-07-28 22:48:52 +0500

    6 kyu added

--- 6kyu.js
+++ 6kyu.js
@@ -45,3 +45,19 @@
 // console.log(sumDigPow(1, 100));
 
 // ===========================================================================
+
+// 3.
+// Question:
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+// The tests contain some very huge arrays, so think about performance.
+// This is the first kata in series:
+// 1. Find the unique number (this kata)
+// 2. Find the unique string
+// 3. Find The Unique
+
+// Answare:
+// let findUniq = (arr) => arr.find((v) => arr.lastIndexOf(v) === arr.indexOf(v));
+// console.log(findUniq([4, 4, "foo", 4]));
