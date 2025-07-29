bb4e443017bcc11e80d4a798775c459f0da6e9d4
FozilAzimov <foziljonweb@gmail.com>
2025-07-29 18:25:29 +0500

    6 kyu added

--- 6kyu.js
+++ 6kyu.js
@@ -92,3 +92,14 @@
 // console.log(expandedForm(70304));
 
 // ===========================================================================
+
+// 5.
+// Question:
+// Implement a function which behaves like the uniq command in UNIX.
+// It takes as input a sequence and returns a sequence in which all duplicate elements following each other have been reduced to one instance.
+// Example:
+// ["a", "a", "b", "b", "c", "a", "b", "c"]  =>  ["a", "b", "c", "a", "b", "c"]
+
+// Answare:
+// let uniq = (a) => a.filter((v, i) => a[i] !== a[i + 1]);
+// console.log(uniq(["a", "a", "b", "b", "c", "a", "b", "c"]));
