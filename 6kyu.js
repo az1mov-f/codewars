8e2403c025e45141b11304397b6fdacc0566a2ac
FozilAzimov <foziljonweb@gmail.com>
2025-07-30 23:55:31 +0500

    6 kyu added

--- 6kyu.js
+++ 6kyu.js
@@ -203,3 +203,28 @@
 //     [0, 0, 0, 0],
 //   ])
 // );
+
+// ===========================================================================
+
+// 7.
+// https://www.codewars.com/kata/59daf400beec9780a9000045/javascript
+// Question:
+
+// What's in a name?
+// ...Or rather, what's a name in? For us, a particular string is where we are looking for a name.
+// Task
+// Write a function, taking two strings in parameter,
+// that tests whether or not the first string contains all of the letters of the second string, in order.
+// The function should return true if that is the case, and else false. Letter comparison should be case-INsensitive.
+
+// Answare:
+// let nameInStr = (str, name) => {
+//   str = str.toLowerCase();
+//   name = name.toLowerCase();
+//   let i = 0;
+//   for (let val of str) if (val === name[i]) i++;
+//   return i === name.length;
+// };
+// console.log(nameInStr("Across the rivers", "chris"));
+
+// ===========================================================================
