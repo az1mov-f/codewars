c049547219a09c7fa47051dd5570d8f57fcb114a
FozilAzimov <foziljonweb@gmail.com>
2025-07-31 19:48:44 +0500

    6 kyu added

--- 6kyu.js
+++ 6kyu.js
@@ -263,3 +263,19 @@
 // };
 // console.log(sequence((n) => n).takeWhile((n) => n < 10));
 // console.log(sequence((n) => n * n).take(10));
+
+// ===========================================================================
+
+// 9.
+// https://www.codewars.com/kata/55a5c82cd8e9baa49000004c/javascript
+// Question:
+// Complete the function that takes 3 numbers x, y and k (where x ≤ y),
+// and returns the number of integers within the range [x..y] (both ends included) that are divisible by k.
+// More scientifically: { i : x ≤ i ≤ y, i mod k = 0 }
+// Example
+// Given x = 6, y = 11, k = 2 the function should return 3, because there are three numbers divisible by 2 between 6 and 11: 6, 8, 10
+// Note: The test cases are very large. You will need a O(log n) solution or better to pass. (A constant time solution is possible.)
+
+// Answare:
+// let divisibleCount = (x, y, k) => Math.floor(y / k) - Math.floor((x - 1) / k);
+// console.log(divisibleCount(6, 11, 2));
