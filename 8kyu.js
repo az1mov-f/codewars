2679f52996c9c5fa1280470efef486cf925a59c8
FozilAzimov <foziljonweb@gmail.com>
2025-08-17 14:36:50 +0500

    8 kyu added

--- 8kyu.js
+++ 8kyu.js
@@ -38,3 +38,18 @@
 // mergeArrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2]);
 
 // ===========================================================================
+
+// 2.
+// Question:
+// https://www.codewars.com/kata/57a55c8b72292d057b000594/javascript
+
+// You need to write a function that reverses the words in a given string. Words are always separated by a single space.
+// As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+// Example (Input --> Output)
+// "Hello World" --> "World Hello"
+// "Hi There." --> "There. Hi"
+// Happy coding!
+
+// Answare:
+// let reverse = (str) => str.split(" ").reverse().join(" ");
+// console.log(reverse("I am an expert at this"));
