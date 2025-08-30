c4d218b5b6ec235747cf85197984fefa066b13d3
FozilAzimov <foziljonweb@gmail.com>
2025-08-30 21:34:39 +0500

    4 kyu added

---
+++ 4kyu.js
@@ -0,0 +1,14 @@
+// 1.
+// Question:
+// https://www.codewars.com/kata/5324945e2ece5e1f32000370/javascript
+
+// Answare:
+// let sumStrings = (a, b) => `${BigInt(a) + BigInt(b)}`;
+
+// console.log(sumStrings("1", "2"));
+// console.log(
+//   sumStrings("712569312664357328695151392", "8100824045303269669937")
+// );
+// console.log(
+//   sumStrings("50095301248058391139327916261", "81055900096023504197206408605")
+// );
