5ba372d962dda61c0a665f84d4fe4b8681052a7a
FozilAzimov <foziljonweb@gmail.com>
2025-09-04 23:58:02 +0500

    8 kyu added

--- 8kyu.js
+++ 8kyu.js
@@ -327,3 +327,15 @@
 // console.log(array(""));
 
 // ===========================================================================
+
+// 16.
+// Question:
+// https://www.codewars.com/kata/5ab6538b379d20ad880000ab/javascript
+
+// Answare:
+// const areaOrPerimeter = (l, w) => (l == w ? l * w : 2 * (l + w));
+
+// console.log(areaOrPerimeter(3, 3));
+// console.log(areaOrPerimeter(6, 10));
+
+// ===========================================================================
